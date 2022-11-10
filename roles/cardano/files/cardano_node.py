e5bdabf1bb7d2722804b6d812ce8acade1a69d08
Óscar Nájera <hi@oscarnajera.com>
2022-11-10 15:27:59 +0100

    transfer preview node to same host

--- roles/cardano/files/cardano_node.py
+++ roles/cardano/files/cardano_node.py
@@ -5,11 +5,13 @@ import json
 
 
 def discover_cardano_node(section):
-    yield Service()
+    for network, _ in zip(["mainnet", "preview"], section):
+        yield Service(item=network)
 
 
-def check_cardano_node(section):
-    metrics = json.loads(section[0][0])["cardano"]["node"]["metrics"]
+def check_cardano_node(item, section):
+    network = 0 if item == "mainnet" else 1
+    metrics = json.loads(section[network][0])["cardano"]["node"]["metrics"]
     mem_used = metrics["Mem"]["resident"]["int"]["val"]
     mem_live = metrics["RTS"]["gcLiveBytes"]["int"]["val"]
     mem_heap = metrics["RTS"]["gcHeapBytes"]["int"]["val"]
@@ -17,7 +19,9 @@ def check_cardano_node(section):
     yield Metric("mem_nonheap", mem_live)
     yield Metric("mem_heap", mem_heap)
 
-    chain_density = float(metrics["density"]["real"]["val"]) * 100
+    chain_density = (
+        float(metrics.get("density", {"real": {"val": 0}})["real"]["val"]) * 100
+    )
     yield Metric("chain_density", chain_density, levels=(6, 7), boundaries=(0, 100))
     # print(metrics)
     yield Result(state=State.OK, summary="YOUN")
@@ -25,7 +29,7 @@ def check_cardano_node(section):
 
 register.check_plugin(
     name="cardano_node",
-    service_name="Cardano Node",
+    service_name="Cardano Node %s",
     discovery_function=discover_cardano_node,
     check_function=check_cardano_node,
 )
