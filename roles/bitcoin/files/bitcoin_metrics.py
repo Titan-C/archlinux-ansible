0c3c0c517b5e2ca37780b99fab532fa11468d889
Oscar Najera <hi@oscarnajera.com>
2024-09-20 14:59:20 +0200

    bitcoin metrics nicer

--- roles/bitcoin/files/bitcoin_metrics.py
+++ roles/bitcoin/files/bitcoin_metrics.py
@@ -23,10 +23,38 @@ metric_info["headers"] = {
     "color": "14/a",
 }
 
+metric_info["pruneheight"] = {
+    "title": _("Prune Height"),
+    "unit": "count",
+    "color": "24/a",
+}
+
 graph_info["blocks_n_headers"] = {
     "title": _("Blocks and Headers"),
     "metrics": [
         ("blocks", "area"),
         ("headers", "line"),
+        ("pruneheight", "line"),
+    ],
+}
+
+## LND
+metric_info["num_active_channels"] = {
+    "title": _("Active Channels"),
+    "unit": "count",
+    "color": "16/a",
+}
+
+metric_info["num_peers"] = {
+    "title": _("Peers"),
+    "unit": "count",
+    "color": "26/a",
+}
+
+graph_info["channels_n_peers"] = {
+    "title": _("Active Channels and Peers"),
+    "metrics": [
+        ("num_active_channels", "line"),
+        ("num_peers", "line"),
     ],
 }
